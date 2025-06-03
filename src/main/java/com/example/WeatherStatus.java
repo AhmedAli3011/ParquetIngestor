90038cfd76f68f69cfbaaf92aa593a160d173a9e
AhmedAli3011 <ahmed.moh3011@gmail.com>
2025-06-03 19:56:47 +0300

    Whole Feature

---
+++ src/main/java/com/example/WeatherStatus.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WeatherStatus {
+    private long stationId;
+    @JsonProperty("sNo")
+    private long sNo;
+    private String batteryStatus;
+    private long statusTimestamp;
+    private WeatherData weather;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class WeatherData {
+        private int humidity;
+        private int temperature;
+        private int windSpeed;
+    }
+}
